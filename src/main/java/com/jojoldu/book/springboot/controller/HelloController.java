87c817ecf02d5ba82141e75f8de72224602c3c0c
Yoojin Lee <yoojinLee.dev@gmail.com>
2022-03-08 00:47:30 +0900

    Test: Add helloDto method
    helloDto 메서드 생성

--- src/main/java/com/jojoldu/book/springboot/controller/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/controller/HelloController.java
@@ -1,13 +1,21 @@
 package com.jojoldu.book.springboot.controller;
 
+import com.jojoldu.book.springboot.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //
+@RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    /*DTO를 통해서 넘겨주는 함수*/
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String _name, @RequestParam("amount") int _amount) {
+        return new HelloResponseDto(_name, _amount);
     }
 }
