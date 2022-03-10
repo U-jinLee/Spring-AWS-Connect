1b8d7a2bc75c961fb2a5828b706316848e393551
Yoojin Lee <yoojinLee.dev@gmail.com>
2022-03-10 21:26:11 +0900

    Test: PostsRepository
    JPA를 활용한 PostsRepository.java 생성

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -2,5 +2,6 @@ package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/*단순히 테이블 설계만으로는 존재할 수 없음 아래와 같이 설정을 해줘야 가능*/
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 }
