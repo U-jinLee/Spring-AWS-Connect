2f995ef9e680e49bf3b4c882f94bf2873178726a
Yoojin Lee <yoojinLee.dev@gmail.com>
2022-03-12 00:13:36 +0900

    Feat: PostsUpdateRequestDto.java commit
    UpdateRequestDto 추

---
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,18 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+    private String title;
+    private String content;
+
+    @Builder
+    public PostsUpdateRequestDto(String _title, String _content) {
+        this.title = _title;
+        this.content = _content;
+    }
+}
