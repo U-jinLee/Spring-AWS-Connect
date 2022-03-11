2564a13556b46c0622f943a2ee569da74738c549
Yoojin Lee <yoojinLee.dev@gmail.com>
2022-03-11 23:24:54 +0900

    Style: Add web package
    web Package 추가

--- src/main/java/com/jojoldu/book/springboot/web/controller/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/controller/HelloController.java
@@ -1,6 +1,6 @@
-package com.jojoldu.book.springboot.controller;
+package com.jojoldu.book.springboot.web.controller;
 
-import com.jojoldu.book.springboot.dto.HelloResponseDto;
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
