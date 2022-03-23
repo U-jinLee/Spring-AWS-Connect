edf47fd270e1714769fd67dede9f181b3480d6b1
Yoojin Lee <yoojinLee.dev@gmail.com>
2022-03-24 00:09:49 +0900

    Feat: GetMapping index 메서드 추가

--- src/main/java/com/jojoldu/book/springboot/web/controller/indexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/controller/indexController.java
@@ -1,4 +1,12 @@
 package com.jojoldu.book.springboot.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class indexController {
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
 }
